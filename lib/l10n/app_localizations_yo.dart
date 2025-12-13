a0a3e84bfac79cc2288394450a0ac6e531a40cfa
Auwal rg <auwalrg8@gmail.com>
2025-12-13 17:10:26 +0100

    updates

---
+++ lib/l10n/app_localizations_yo.dart
@@ -0,0 +1,404 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Yoruba (`yo`).
+class AppLocalizationsYo extends AppLocalizations {
+  AppLocalizationsYo([String locale = 'yo']) : super(locale);
+
+  @override
+  String get appTitle => 'Sabi Wallet';
+
+  @override
+  String get welcome => 'Kaabo';
+
+  @override
+  String get welcomeDescription =>
+      'Pa Bitcoin rẹ mọ ni ailewu. Ko si ẹnikan ti o le dena owo rẹ tabi di akọọlẹ rẹ.';
+
+  @override
+  String get letsStart => 'Jẹ ki a bẹrẹ Sabi ₿';
+
+  @override
+  String get restore => 'Mu pada';
+
+  @override
+  String get totalBalance => 'Apapọ Owo';
+
+  @override
+  String get sats => 'sats';
+
+  @override
+  String get send => 'Firanṣẹ';
+
+  @override
+  String get receive => 'Gba';
+
+  @override
+  String get transactions => 'Awọn Iṣowo';
+
+  @override
+  String get airtime => 'Ayelujara';
+
+  @override
+  String get payBills => 'Sanwo Owo';
+
+  @override
+  String get recentTransactions => 'Awọn Iṣowo Ológ';
+
+  @override
+  String get seeAll => 'Wò Gbogbo';
+
+  @override
+  String get failedToLoadTransactions => 'Kuna sise nini awọn iṣowo';
+
+  @override
+  String get receivedPayment => 'Ọ Gba Owo';
+
+  @override
+  String get sentPayment => 'Ọ Firanṣẹ Owo';
+
+  @override
+  String get chooseRecipient => 'Yan Olugba';
+
+  @override
+  String get enterAmount => 'Tẹ Iye sii';
+
+  @override
+  String get lightning => 'Lightning';
+
+  @override
+  String get bitcoin => 'Bitcoin';
+
+  @override
+  String get account => 'Akáwo';
+
+  @override
+  String get preferences => 'Awọn Ẹniti O Fẹran';
+
+  @override
+  String get cash => 'Owo';
+
+  @override
+  String get zaps => 'Zaps';
+
+  @override
+  String get profile => 'Profaili';
+
+  @override
+  String get settings => 'Eto';
+
+  @override
+  String get language => 'Ede';
+
+  @override
+  String get currency => 'Owo Orile-ede';
+
+  @override
+  String get biometric => 'Biometric';
+
+  @override
+  String get notifications => 'Awọn ikede';
+
+  @override
+  String get networkFee => 'Owo Nẹtiwọọki';
+
+  @override
+  String get transactionLimit => 'Opin Iṣowo';
+
+  @override
+  String get english => 'Gẹẹsi';
+
+  @override
+  String get hausa => 'Hausa';
+
+  @override
+  String get yoruba => 'Yoruba';
+
+  @override
+  String get pidgin => 'Pidgin';
+
+  @override
+  String get amount => 'Iye';
+
+  @override
+  String get fees => 'Awọn owo';
+
+  @override
+  String get time => 'Akoko';
+
+  @override
+  String get status => 'Ipo';
+
+  @override
+  String get type => 'Iru';
+
+  @override
+  String get description => 'Apejuwe';
+
+  @override
+  String get invoice => 'Iwe-ifowopamọ';
+
+  @override
+  String get completed => 'Ti pari';
+
+  @override
+  String get pending => 'N duro';
+
+  @override
+  String get failed => 'O kuna';
+
+  @override
+  String get sent => 'Ti firanṣẹ';
+
+  @override
+  String get received => 'Ti gba';
+
+  @override
+  String get today => 'Loni';
+
+  @override
+  String get yesterday => 'Lana';
+
+  @override
+  String get at => 'ni';
+
+  @override
+  String get continueButton => 'Tẹsiwaju';
+
+  @override
+  String get cancel => 'Fagilee';
+
+  @override
+  String get confirm => 'Jẹrisi';
+
+  @override
+  String get done => 'Ti pari';
+
+  @override
+  String get retry => 'Tun gbiyanju';
+
+  @override
+  String get exit => 'Jade';
+
+  @override
+  String get share => 'Pin';
+
+  @override
+  String get copyToClipboard => 'Daakọ si clipboard';
+
+  @override
+  String copiedToClipboard(String item) {
+    return '$item ti daakọ si clipboard';
+  }
+
+  @override
+  String get enterRecipient => 'Tẹ adirẹsi olugba tabi invoice sii';
+
+  @override
+  String get scanQR => 'Scan QR';
+
+  @override
+  String get paste => 'Lẹ';
+
+  @override
+  String get paymentSuccess => 'Isanwo Ṣaṣeyọri';
+
+  @override
+  String get paymentFailed => 'Isanwo Kuna';
+
+  @override
+  String get paymentPending => 'Isanwo N duro';
+
+  @override
+  String get paymentSentHeadline =>
+      'Sisan ti ja! A ṣi ń ṣọ ipo rẹ́ ní abẹ́lẹ́.';
+
+  @override
+  String get shareOptionsTitle => 'Báwo lẹ fẹ́ pín?';
+
+  @override
+  String get shareImage => 'Pín gẹ́gẹ́ bi Aworan';
+
+  @override
+  String get sharePdf => 'Pín gẹ́gẹ́ bi PDF';
+
+  @override
+  String get shareImageSubtitle => 'Firanṣẹ́ àwòrán ìwé ìrísí';
+
+  @override
+  String get sharePdfSubtitle => 'Ṣẹda PDF ìkólẹ̀kí àti firanṣẹ́';
+
+  @override
+  String get recipient => 'Olùgbà';
+
+  @override
+  String get identifier => 'Àpẹẹrẹ';
+
+  @override
+  String get transactionTime => 'Àkókò ìbánisọ̀rọ̀';
+
+  @override
+  String get memo => 'Àkọsílẹ̀';
+
+  @override
+  String get backToHome => 'Padà sí ilé';
+
+  @override
+  String get shareReceipt => 'Pín ìwé ìrísí';
+
+  @override
+  String get createInvoice => 'Ṣẹda Invoice';
+
+  @override
+  String get generateInvoice => 'Ṣe Invoice';
+
+  @override
+  String get shareInvoice => 'Pin Invoice';
+
+  @override
+  String get backupWallet => 'Ṣe afipamọ Wallet';
+
+  @override
+  String get recoveryPhrase => 'Awọn ọrọ Imudani';
+
+  @override
+  String get seedPhrase => 'Awọn ọrọ Irugbin';
+
+  @override
+  String get writeDownSeedPhrase => 'Kọ awọn ọrọ irugbin rẹ silẹ';
+
+  @override
+  String get verifySeedPhrase => 'Jẹrisi awọn ọrọ irugbin';
+
+  @override
+  String get editProfile => 'Ṣatunkọ Profaili';
+
+  @override
+  String get displayName => 'Orukọ';
+
+  @override
+  String get phoneNumber => 'Nọmba Foonu';
+
+  @override
+  String get email => 'Imeeli';
+
+  @override
+  String get security => 'Aabo';
+
+  @override
+  String get privacy => 'Aṣiri';
+
+  @override
+  String get about => 'Nipa';
+
+  @override
+  String get help => 'Iranlọwọ';
+
+  @override
+  String get logout => 'Jade';
+
+  @override
+  String get switchWallet => 'Yipada Wallet';
+
+  @override
+  String get createNewWallet => 'Ṣẹda Wallet Tuntun';
+
+  @override
+  String get restoreWallet => 'Mu Wallet pada';
+
+  @override
+  String get error => 'Aṣiṣe';
+
+  @override
+  String get success => 'Aṣeyọri';
+
+  @override
+  String get warning => 'Ikilọ';
+
+  @override
+  String get info => 'Alaye';
+
+  @override
+  String get loading => 'N ṣiṣẹ...';
+
+  @override
+  String get pleaseWait => 'Jọwọ duro';
+
+  @override
+  String get processing => 'N ṣiṣẹ...';
+
+  @override
+  String get lightningNetwork => 'Lightning Network';
+
+  @override
+  String get bitcoinAddress => 'Adirẹsi Bitcoin';
+
+  @override
+  String get contacts => 'Awọn olubasọrọ';
+
+  @override
+  String get recentContacts => 'Awọn Olubasọrọ Laipe';
+
+  @override
+  String get addContact => 'Fi Olubasọrọ kun';
+
+  @override
+  String get quickAmounts => 'Awọn Iye Yara';
+
+  @override
+  String get customAmount => 'Iye Ti ara ẹni';
+
+  @override
+  String get livePrice => 'Idiyele Lọwọlọwọ';
+
+  @override
+  String get buyRate => 'Oṣuwọn Rira';
+
+  @override
+  String get sellRate => 'Oṣuwọn Tita';
+
+  @override
+  String get tapToSwitch => 'Tẹ lati yipada owo';
+
+  @override
+  String get liveMarketRate => 'Oṣuwọn ọja lọwọlọwọ';
+
+  @override
+  String get exitWallet => 'Jade kuro ninu Sabi Wallet';
+
+  @override
+  String get exitWalletConfirm => 'Ṣe o fẹ jade kuro ninu Sabi Wallet nitootọ?';
+
+  @override
+  String get noTransactions => 'Ko si awọn iṣowo ṣibẹ';
+
+  @override
+  String get noNotifications => 'Ko si awọn ikede';
+
+  @override
+  String get paymentReceived => 'Isanwo Ti gba';
+
+  @override
+  String get paymentSent => 'Isanwo Ti firanṣẹ';
+
+  @override
+  String get insufficientBalance => 'Owo ko to';
+
+  @override
+  String get invalidAddress => 'Adirẹsi ko tọ';
+
+  @override
+  String get invalidInvoice => 'Invoice ko tọ';
+
+  @override
+  String get economy => 'Eto-ọrọ aje';
+
+  @override
+  String get normal => 'Deede';
+
+  @override
+  String get priority => 'Pataki';
+}
